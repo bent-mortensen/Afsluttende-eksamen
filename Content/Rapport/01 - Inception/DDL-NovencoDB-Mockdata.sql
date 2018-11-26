f651cc98f066106d6c52da6705f3940aeb2975f1
Bent Sunesen Mortensen <kooogt@gmail.com>
2018-11-26 15:46:52 +0100

    Mandagens arbejde

--- Content/Rapport/01 - Inception/DDL-NovencoDB-Mockdata.sql
+++ Content/Rapport/01 - Inception/DDL-NovencoDB-Mockdata.sql
@@ -40,6 +40,7 @@ CREATE TABLE Employee (
 	FK_Company_id INT FOREIGN KEY REFERENCES Company(Company_id)
 );
 
+
 CREATE TABLE Ventilator_status (
 	Ventilator_status_id INT IDENTITY(1,1) PRIMARY KEY,
 	[Datetime] DATETIME,
@@ -51,17 +52,43 @@ CREATE TABLE Ventilator_status (
 	FK_Ventilator_id INT FOREIGN KEY REFERENCES Ventilator(Ventilator_id)
 );
 
+
+CREATE TABLE Error_type(
+	Error_type_id INT IDENTITY(1,1) PRIMARY KEY,
+	[Type_name] NVARCHAR(255)
+);
+
+
 CREATE TABLE Ventilator_error (
 	Ventilator_error_id INT IDENTITY(1,1) PRIMARY KEY,
-	[Type] NVARCHAR(255),
+	FK_Error_type_id INT FOREIGN KEY REFERENCES Error_type(Error_type_id),
 	FK_Ventilator_status_id INT FOREIGN KEY REFERENCES Ventilator_status(Ventilator_status_id)
+);
+
+
+CREATE TABLE Spare_part(
+	Spare_part_id INT IDENTITY(1,1) PRIMARY KEY,
+	Spare_part_name NVARCHAR(255)
+);
+
+CREATE TABLE Spare_part_list(
+	Spare_part_list_id INT IDENTITY(1,1) PRIMARY KEY,
+	List_id INT,
+	FK_Spare_part_id INT FOREIGN KEY REFERENCES Spare_part(Spare_part_id)
 );
 
 CREATE TABLE Error_correction_report (
 	Error_correction_report_id INT IDENTITY(1,1) PRIMARY KEY,
-	[Description] NVARCHAR(255),
+	Error_description NVARCHAR(4000),
+	Error_correction_description NVARCHAR(4000),
+	Correction_date DATETIME,
+	Sap_celcius NVARCHAR(255),
+	Sap_hertz NVARCHAR(255),
+	Sap_kwh NVARCHAR(255),
+	Sap_amps NVARCHAR(255),
+	FK_Spare_part_list_id INT FOREIGN KEY REFERENCES Spare_part_list(Spare_part_list_id),
 	FK_Employee_id INT FOREIGN KEY REFERENCES Employee(Employee_id),
-	FK_Ventilator_status_id INT FOREIGN KEY REFERENCES Ventilator_status(Ventilator_status_id)
+	FK_Ventilator_error_id INT FOREIGN KEY REFERENCES Ventilator_error(Ventilator_error_id)
 );
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -88,7 +115,7 @@ SELECT * FROM Ventilator;
 INSERT INTO Employee ([Name], Phonenumber, Email, FK_Company_id) VALUES ('bent mortensen', 22845214, 'bent_mortensen4@hotmail.com', 1);
 
 SELECT * FROM Employee;
-DELETE FROM Employee WHERE Name='Test';
+--DELETE FROM Employee WHERE Name='Test';
 
 
 INSERT INTO Ventilator_status ([Datetime], Celcius, Hertz, kWh, Amps, Validated, FK_Ventilator_id) VALUES (CURRENT_TIMESTAMP, 45, 4, 4 , 2, 'valid', 1);
@@ -98,14 +125,36 @@ INSERT INTO Ventilator_status ([Datetime], Celcius, Hertz, kWh, Amps, Validated,
 SELECT * FROM Ventilator_status;
 -- DELETE FROM Ventilator_status WHERE Hertz=70;
 
-INSERT INTO Ventilator_error ([Type], FK_Ventilator_status_id) VALUES ('Type of Error Celcius, Hertz, kWh, Amps - maybe a small description of error types it have', 1);
+INSERT INTO Error_type ([Type_name]) VALUES ('Rystelser - Hertz');
+INSERT INTO Error_type ([Type_name]) VALUES ('Temperatur - Celcius');
+INSERT INTO Error_type ([Type_name]) VALUES ('Ampere - Amps');
+INSERT INTO Error_type ([Type_name]) VALUES ('Kilowatt-timer - kWh');
+
+SELECT * FROM Error_type;
+
+INSERT INTO Ventilator_error (FK_Error_type_id, FK_Ventilator_status_id) VALUES (1, 1);
 
 SELECT * FROM Ventilator_error;
 -- clean up
 -- DELETE FROM Ventilator_error WHERE Ventilator_error_id BETWEEN 13 AND 183;
 
 
-INSERT INTO Error_correction_report ([Description], FK_Employee_id, FK_Ventilator_status_id) VALUES ('En beskrivelse af fejlen, samt en beskrivelse af tiltag lavet for at rette fejlen!', 1, 1);
+INSERT INTO Spare_part (Spare_part_name) VALUES ('None');
+INSERT INTO Spare_part (Spare_part_name) VALUES ('lejer');
+INSERT INTO Spare_part (Spare_part_name) VALUES ('ravpluks');
+INSERT INTO Spare_part (Spare_part_name) VALUES ('motor');
+INSERT INTO Spare_part (Spare_part_name) VALUES ('varmeskjold');
+INSERT INTO Spare_part (Spare_part_name) VALUES ('Andet');
+INSERT INTO Spare_part (Spare_part_name) VALUES ('føler');
+
+SELECT * FROM Spare_part;
+
+INSERT INTO Spare_part_list (List_id,FK_Spare_part_id) VALUES (1, 1);
+INSERT INTO Spare_part_list (List_id,FK_Spare_part_id) VALUES (1, 6);
+
+SELECT * FROM Spare_part_list WHERE List_id = 1;
+
+INSERT INTO Error_correction_report (Error_description, Error_correction_description, Correction_date, Sap_celcius, Sap_amps, Sap_hertz, Sap_kwh, FK_Ventilator_error_id, FK_Employee_id, FK_Spare_part_list_id) VALUES ('beskrivelse af fejlen', 'beskrivelse af tiltag for at rette fejlen', CURRENT_TIMESTAMP, 0, 0, 0, 0, 1, 1, 1);
 
 SELECT *FROM Error_correction_report;
 
